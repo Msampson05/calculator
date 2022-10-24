e9f1422e16b77d6fba8e94a1af0dcb0f2f8bbd54
Megan Sampson <msampson@nyc.yearup.org>
2022-10-24 09:16:15 -0400

    made changes

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+"use strict";
+window.onload = init;
+function init() {
+    // Find HTML elements 
+
+    const number1Field = document.getElementById("number1Field");
+    
+    const number2Field = document.getElementById("number2Field");
+    
+
+     // Buttons
+    const addBtn = document.getElementById("addBtn");
+    addBtn.onclick = onAddBtnClicked; 
+
+    const subBtn = document.getElementById("subBtn");
+    addBtn.onclick = onSubBtnClicked; 
+
+    const mulBtn = document.getElementById("mullBtn");
+    addBtn.onclick = onMulBtnClicked;
+
+    const divBtn = document.getElementById("divBtn");
+    addBtn.onclick = onDivBtnClicked; 
+
+    
+
+    // Get user inputs 
+
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
+
+    // Use the values 
+
+    // display the results for Add
+    
+}
+function onAddBtnClicked() {
+    let answer = number1 + number2;
+    const answerField = document.getElementById("answerField");
+    answerField.value = answer;
+}
+
+
+
