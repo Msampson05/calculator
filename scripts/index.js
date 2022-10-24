e06144697254170c53bf0a46905a68e3840c8c31
Megan Sampson <msampson@nyc.yearup.org>
2022-10-24 09:35:31 -0400

    completed add function

--- scripts/index.js
+++ scripts/index.js
@@ -13,20 +13,19 @@ function init() {
     addBtn.onclick = onAddBtnClicked; 
 
     const subBtn = document.getElementById("subBtn");
-    addBtn.onclick = onSubBtnClicked; 
+    //addBtn.onclick = onSubBtnClicked; 
 
     const mulBtn = document.getElementById("mullBtn");
-    addBtn.onclick = onMulBtnClicked;
+    //addBtn.onclick = onMulBtnClicked;
 
     const divBtn = document.getElementById("divBtn");
-    addBtn.onclick = onDivBtnClicked; 
+    //addBtn.onclick = onDivBtnClicked; 
 
     
 
     // Get user inputs 
 
-    let number1 = Number(number1Field.value);
-    let number2 = Number(number2Field.value);
+  
 
     // Use the values 
 
@@ -34,6 +33,8 @@ function init() {
     
 }
 function onAddBtnClicked() {
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
     let answer = number1 + number2;
     const answerField = document.getElementById("answerField");
     answerField.value = answer;
